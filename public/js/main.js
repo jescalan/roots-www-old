fb222d6633f1b2c29f256270cec8e647170d269c
Jeff Escalante <hello@jenius.me>
2013-06-23 18:10:26 -0400

    rewrite dynamic content docs, fix plugin api docs, add docs for multipass compiles

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,54 @@
-(function(){requirejs.config({shim:{waypoints:{deps:["jquery"]}},paths:{jquery:"//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min",waypoints:"/js/waypoints.min"}}),require(["jquery","waypoints"],function(e){return e(function(){return e(".icon").addClass("in"),e(window).scrollTop()>392&&e("#docnav").css({top:5}),e(window).on("scroll",function(){return e(window).scrollTop()>330?e("#docnav").css({top:5}):e("#docnav").css({top:330-e(window).scrollTop()})}),e(".docs h3").waypoint(function(){return e("#docnav li").removeClass("active"),e("a[href='#"+e(this).attr("id")+"']").parent().addClass("active")}),e("a[href*=#]:not([href=#])").on("click",function(){var t,n;if(location.pathname.replace(/^\//,"")===this.pathname.replace(/^\//,"")||location.hostname===this.hostname){n=e(this.hash).length?e(this.hash):e("[name="+this.hash.slice(1)+"]");if(n.length)return t=n.offset().top>e(window).scrollTop()?-1:1,e("html,body").animate({scrollTop:n.offset().top-t},500),!1}}),e('a[href^="http://"]').attr("target","_blank")})})}).call(this)
+(function() {
+  requirejs.config({
+    shim: {
+      waypoints: {
+        deps: ['jquery']
+      }
+    },
+    paths: {
+      jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min',
+      waypoints: '/js/waypoints.min'
+    }
+  });
+
+  require(['jquery', 'waypoints'], function($) {
+    return $(function() {
+      $('.icon').addClass('in');
+      if ($(window).scrollTop() > 392) {
+        $('#docnav').css({
+          top: 5
+        });
+      }
+      $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 330) {
+          return $('#docnav').css({
+            top: 5
+          });
+        } else {
+          return $('#docnav').css({
+            top: 330 - $(window).scrollTop()
+          });
+        }
+      });
+      $('.docs h3').waypoint(function() {
+        $('#docnav li').removeClass('active');
+        return $("a[href='#" + ($(this).attr('id')) + "']").parent().addClass('active');
+      });
+      $('a[href*=#]:not([href=#])').on('click', function() {
+        var factor, target;
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname === this.hostname) {
+          target = $(this.hash).length ? $(this.hash) : $("[name=" + (this.hash.slice(1)) + "]");
+          if (target.length) {
+            factor = target.offset().top > $(window).scrollTop() ? -1 : 1;
+            $('html,body').animate({
+              scrollTop: target.offset().top - factor
+            }, 500);
+            return false;
+          }
+        }
+      });
+      return $('a[href^="http://"]').attr('target', '_blank');
+    });
+  });
+
+}).call(this);
